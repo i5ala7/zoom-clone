0d7dd27774bc3a267e991a4a041b23787612ac03
Salah Merouani <sala7merouani@gmail.com>
2022-05-30 01:04:13 +0100

    Room Id added to socket info

--- server.js
+++ server.js
@@ -1,9 +1,46 @@
 const express = require("express");
+const req = require("express/lib/request");
+const res = require("express/lib/response");
+const { Socket } = require("socket.io");
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const {v4} = require('uuid');
 
-const port =5000;
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
 
-app.listen(port, ()=> {
-    console.log("server running on port:", port);
+
+
+
+const randomId = v4() ;
+console.log('This is a random Id', randomId)
+
+
+app.get('/', (req, res) => {
+    res.redirect('/'+ randomId)
+    
+})
+
+
+
+
+app.get('/:room', (req, res) => {
+    res.render ('room', {roomId: req.params.room})
+})
+
+
+
+ io.on('connection', socket => {
+    socket.on('join-room', (roomId , userId) => {
+        console.log(roomId, userId)
+   })
+ })
+
+server.listen(3000, ()=> {
+
+   
+
+    console.log('server running on port 3000')
 })
